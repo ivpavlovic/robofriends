24f3323287909e4c41e7438b153f991e47bf9cec
Marco <marko.pav@gmail.com>
2023-01-23 19:22:30 +0100

    Updates

---
+++ precache-manifest.6d05b7817a338dc16909f73861f57d03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c812e87e9335228c7ec3",
+    "url": "/robofriends/static/css/main.a6e52464.chunk.css"
+  },
+  {
+    "revision": "c812e87e9335228c7ec3",
+    "url": "/robofriends/static/js/main.4e3a13ed.chunk.js"
+  },
+  {
+    "revision": "cb45653fdb1cd181cc7c",
+    "url": "/robofriends/static/js/runtime~main.465d8a73.js"
+  },
+  {
+    "revision": "61f55b18e6aea6750762",
+    "url": "/robofriends/static/css/2.ad2bb246.chunk.css"
+  },
+  {
+    "revision": "61f55b18e6aea6750762",
+    "url": "/robofriends/static/js/2.39830d21.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/robofriends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c8d29aa163a9b5333d06c65da4319459",
+    "url": "/robofriends/index.html"
+  }
+];
